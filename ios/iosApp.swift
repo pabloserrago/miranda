f6d0c138496cdcf69f0ea1e2e3c2b04aa8db1982
Pablo Serrano Gomez <pserrano@duckduckgo.com>
2025-10-27 21:09:59 +0100

    Initial Commit

---
+++ ios/iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  iosApp.swift
+//  ios
+//
+//  Created by Pablo Serrano on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iosTests/iosTests.swift
@@ -0,0 +1,16 @@
+//
+//  iosTests.swift
+//  iosTests
+//
+//  Created by Pablo Serrano on 10/27/25.
+//
+
+import Testing
+
+struct iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
